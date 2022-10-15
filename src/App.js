6793a9991e39d67b7e7fed77ee3c946ac72f1a51
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-15 17:37:25 -0600

    Update redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store";
+
 function App() {
-  return <div className="App"></div>;
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch(actions.increment());
+  };
+  const decrement = () => {
+    dispatch(actions.decrement());
+  };
+
+  const addBy = () => {
+    dispatch(actions.addBy(10));
+  };
+
+  const minusBy = () => {
+    dispatch(actions.minusBy(10));
+  };
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add by 10</button>
+      <button onClick={minusBy}>Minus by 10</button>
+    </div>
+  );
 }
 
 export default App;
